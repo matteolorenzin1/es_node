d622bf289b5b65898f9ba3e493e00b0f6212aa27
matteolorenzin1 <lorenzin.matteo@studenti.salesianisesto.it>
2023-01-26 09:39:14 +0100

    add express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("c");
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+app.get('/nome', function (req, res) {
+    res.send('ciao ')
+  });
+  
+  app.get('/mioNome', function (req, res) {
+    let nome;
+    nome = req.query.nome;
+    res.send('ciao ' + nome);
+  });
+
+app.listen(3000);
+
