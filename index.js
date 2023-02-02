0227eb8308a8ed3903ba2f86152b40f02e466cdb
matteolorenzin1 <lorenzin.matteo@studenti.salesianisesto.it>
2023-02-02 09:18:04 +0100

    add operators

--- index.js
+++ index.js
@@ -15,5 +15,48 @@ app.get('/nome', function (req, res) {
     res.send('ciao ' + nome);
   });
 
+  app.get('/somma', function (req, res) {
+    let n1;
+    let n2;
+    n1 = req.query.n1;
+    n2 = req.query.n2;
+    let operator = parseInt(n1) + parseInt(n2);
+    res.send('somma ' + operator);
+  });
+
+  app.get('/sottrazione', function (req, res) {
+    let n1;
+    let n2;
+    n1 = req.query.n1;
+    n2 = req.query.n2;
+    let operator = n1 - n2;
+    res.send('sottrazione' + operator);
+  });
+
+  app.get('/moltiplicazione', function (req, res) {
+    let n1;
+    let n2;
+    n1 = req.query.n1;
+    n2 = req.query.n2;
+    let operator = n1 * n2;
+    res.send('moltiplicazione' + operator);
+  });
+
+  app.get('/divisione', function (req, res) {
+    let n1;
+    let n2;
+    n1 = req.query.n1;
+    n2 = req.query.n2;
+    let operator = n1 / n2;
+    res.send('divisione' + operator);
+  });
+// tutto maiuscolo
+  app.get('/upperCase', function (req, res) {
+    let nome;
+    nome = req.query.nome;
+    var x = nome.toString().toUpperCase();
+    res.send('ciao ' + x);
+  });
+
 app.listen(3000);
 
